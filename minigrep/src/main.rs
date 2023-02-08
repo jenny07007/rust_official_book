12884eda020435fc85ab076b6f4cf9886a9edd79
Jenny <jennyliu07@gmail.com>
2023-02-08 18:13:46 +0800

    I/O project / reading a file(modularity & error handling)

--- minigrep/src/main.rs
+++ minigrep/src/main.rs
@@ -1,12 +1,20 @@
 use std::env;
+use std::process;
+
+use minigrep::Config;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
+    let config = Config::build(&args).unwrap_or_else(|err| {
+        println!("Problem parsing argument: {err}");
+        process::exit(1);
+    });
 
-    let query = &args[1];
-    let file_path = &args[2];
+    println!("Searching for '{}'", config.query);
+    println!("In file '{}'", config.file_path);
 
-    println!("Searching for {query}");
-    println!("In file {file_path}");
-    // dbg!(args);
+    if let Err(e) = minigrep::run(config) {
+        println!("Application error: {e}");
+        process::exit(1);
+    }
 }
